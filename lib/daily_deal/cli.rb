8980d921f3f3a3e42148ca37c6fcfd445f05d646
Rebecca Strauss <rstrauss127@gmail.com>
2018-11-30 12:39:21 +0000

    interface working

--- lib/daily_deal/cli.rb
+++ lib/daily_deal/cli.rb
@@ -4,6 +4,7 @@ class DailyDeal::CLI
   def call
     list_deals
     menu
+    goodbye
   end
 
   def list_deals
@@ -15,13 +16,27 @@ class DailyDeal::CLI
   end
 
   def menu
-    puts "Enter the number of the deal you'd like to know more about"
-    input = gets.strip
-    case input
-    when "1"
-      puts "More info on deal 1..."
-    when "2"
-      puts "more info on deal 2..."
+
+    input = nil
+    while input != "exit"
+        puts "Enter the number of the deal you'd like to know more about, or type list to view deals again, or type exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on deal 1..."
+      when "2"
+        puts "more info on deal 2..."
+      when "list"
+        list_deals
+      else
+        puts "Not sure what you want, type list or exit"
+      end
     end
   end
+
+  def goodbye
+    puts "See you tomorrow for more deals"
+  end
+
+
 end
